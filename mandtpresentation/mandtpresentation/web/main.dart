4c0d82d32bd366ecd1c47b8dd37e30602a949400
zedoax <zedoax@anagrio.net>
2019-10-06 03:14:47 -0400

    Removed and Added files

--- mandtpresentation/mandtpresentation/web/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_web_ui/ui.dart' as ui;
-
-// TODO: change `my_app` to refer to your app package name.
-import 'package:mandtpresentation/main.dart' as app;
-
-main() async {
-  await ui.webOnlyInitializePlatform();
-  app.main();
-}
